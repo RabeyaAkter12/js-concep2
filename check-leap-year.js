e4c28add2836e8b5d2520239bb332282f6f23af1
Rasel Ahmed <raseloracle@gmail.com>
2024-02-27 21:32:44 +0600

    second commit

--- check-leap-year.js
+++ check-leap-year.js
@@ -1,3 +1,9 @@
 function isLeapYear(year){
-    
+    if((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0)){
+        return "Leap Year";
+    }
+    else{
+        return "Not Leap Year";
+    }
 }
+console.log(isLeapYear(2024))
